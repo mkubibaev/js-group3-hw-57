b054b291e23c1c0616c81f9107da015d64e37f99
mkubibaev <mkubibaev@gmail.com>
2019-01-10 22:49:13 +0600

    added main container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
+import CostsManager from "./containers/CostsManager/CostsManager";
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-
-			</div>
+			<CostsManager />
 		);
 	}
 }
---
+++ src/containers/CostsManager/CostsManager.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+class CostsManager extends Component {
+    state = {
+        costTitle: '',
+        costPrice: '',
+        costList: [],
+        sum: 0,
+    };
+
+
+    render() {
+        return (
+            <div className="container">
+
+            </div>
+        )
+    }
+}
+
+export default CostsManager;
